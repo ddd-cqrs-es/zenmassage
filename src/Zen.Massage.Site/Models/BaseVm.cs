8f36027c714b95a9aa35c8d6a9c2c523e8b787bd
AdrianLewis <adrian.lewis@immediate.co.uk>
2016-01-15 14:30:42 +0000

    Added Android companion application written in Typescript. May morph into two apps - one for clients and one for therapists.

---
+++ src/Zen.Massage.Site/Models/BaseVm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zen.Massage.Site.Models
+{
+    public class BaseVm
+    {
+        private string _metaTitle;
+
+        public string Title { get; set; }
+
+        public string MetaTitle
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(_metaTitle))
+                {
+                    return _metaTitle;
+                }
+
+                if (string.IsNullOrEmpty(Title))
+                {
+                    return string.Empty;
+                }
+
+                return $"{Title} | Zen Massage";
+            }
+            set { _metaTitle = value; }
+        }
+
+        public string MetaDescription { get; set; }
+
+        public string MetaKeywords { get; set; }
+    }
+}
