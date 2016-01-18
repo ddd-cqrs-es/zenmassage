e07ccd4963f1dac5ce5dbac99e230d81537e0b48
Adrian Lewis <zen_global@hotmail.com>
2016-01-18 09:28:50 +0000

    More commits.

---
+++ src/Zen.Massage.Site/Controllers/PebbleController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Mvc;
+using Zen.Massage.Site.Models;
+
+namespace Zen.Massage.Site.Controllers
+{
+    public class PebbleController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var model =
+                new BaseVm
+                {
+                    Title = "Application Setup"
+                };
+            return View(model);
+        }
+    }
+}
