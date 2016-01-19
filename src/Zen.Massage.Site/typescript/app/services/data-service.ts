21658908ac683cec04e7240496e5bd2a25950a5d
Adrian Lewis <zen_global@hotmail.com>
2016-01-19 23:28:37 +0000

    Example service is actually building. Just need to resolve the dependency injection and setup a suitable factory.

--- src/Zen.Massage.Site/typescript/app/services/data-service.ts
+++ src/Zen.Massage.Site/typescript/app/services/data-service.ts
@@ -1,6 +1,7 @@
-﻿/*import {Http} from 'angular2/http';
+﻿import {Http, RequestOptionsArgs} from 'angular2/http';
+import {Observable} from 'rxjs/rx';
 
-module demoApp {
+module ZenMassage {
 
     export interface ICustomer {
         id: number;
@@ -13,24 +14,91 @@ module demoApp {
         total: number;
     }
 
-    export class DataService {
+    export interface IDataService {
+        getCustomers(): Observable<ICustomer[]>;
+        getOrders(id: number): Observable<IOrder[]>;
+    }
 
-        constructor(private http: Http) { }
-
-        getCustomers(): ng.IPromise<ICustomer[]> {
-            return this.$http.get('customers.json').then(response => {
-                return response.data;
-            });
+    class Customer implements ICustomer {
+        constructor(private _id: number, private _name: string, private _total: number) {
         }
 
-        getOrder(id: number): ng.IPromise<IOrder[]> {
-            return this.$http.get('orders.json', { data: { id: id } }).then(response => {
-                return response.data;
-            });
+        get id(): number {
+            return this._id;
+        }
+
+        get name(): string {
+            return this._name;
+        }
+
+        get total(): number {
+            return this._total;
         }
     }
 
-    angular.module('demoApp')
-        .service('demoApp.dataService', DataService);
+    class Order implements IOrder {
+        constructor(private _product: string, private _total: number) {
+        }
 
-}*/
+        get product(): string {
+            return this._product;
+        }
+
+        get total(): number {
+            return this._total;
+        }
+    }
+
+    export class DataService implements IDataService {
+
+        constructor(private http: Http) {
+        }
+
+        getCustomers(): Observable<ICustomer[]> {
+            return this.http
+                .get('customers.json')
+                .map(response => {
+                    return response.json();
+                })
+                .map((customers: Array<any>) => {
+                    let results: Array<ICustomer> = [];
+                    if (customers != null) {
+                        customers.forEach(
+                            (customer) => {
+                                results.push(
+                                    new Customer(
+                                        customer.id,
+                                        customer.name,
+                                        customer.total));
+                            });
+                    }
+                    return results;
+                });
+        }
+
+        getOrders(id: number): Observable<IOrder[]> {
+            return this.http
+                .get('orders.json', { body: JSON.stringify({ id: id }) })
+                .map(response => {
+                    return response.json();
+                })
+                .map((orders: Array<any>) => {
+                    let results: Array<IOrder> = [];
+                    if (orders != null) {
+                        orders.forEach(
+                            (order) => {
+                                results.push(
+                                    new Order(
+                                        order.product,
+                                        order.total));
+                            });
+                    }
+                    return results;
+                });
+        }
+    }
+
+//    angular.module('demoApp')
+//        .service('demoApp.dataService', DataService);
+
+}
