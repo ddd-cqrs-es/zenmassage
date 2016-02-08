2cb6e0d0552008fbfd323a6a6a6da1715cc82526
DementedDevil <zen_global@hotmail.com>
2016-02-08 17:10:41 +0000

    Added application level assemblies. These assemblies will be used to unify the write and read repositories in due course.

---
+++ src/Zen.Massage.Application.Interfaces/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zen.Massage.Application.Interfaces
+{
+    public class Class1
+    {
+    }
+}
