4c7a12af0343b4c65eb09b20ebba9a8bc0e56028
DementedDevil <zen_global@hotmail.com>
2016-02-09 21:30:25 +0000

    Added code for initialising NEventStore Added code to register domain and infrastructure types. Added lock files to source control.

--- src/Zen.Infrastructure.ReadRepository/BookingReadRepository.cs
+++ src/Zen.Infrastructure.ReadRepository/BookingReadRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Zen.Infrastructure.ReadRepository.DataAccess;
@@ -36,29 +37,87 @@ namespace Zen.Infrastructure.ReadRepository
             }
         }
 
-        public Task<IEnumerable<IReadBooking>> GetFutureOpenBookings(DateTime cutoffDate, BookingStatus status)
+        public async Task<IEnumerable<IReadBooking>> GetFutureOpenBookings(DateTime cutoffDate, BookingStatus status)
         {
-            throw new NotImplementedException();
+            using (var context = new BookingEntityContext())
+            {
+                var query = await context.Bookings
+                    .Include("TherapistBookings")
+                    .Where(b =>
+                        b.ProposedTime > cutoffDate &&
+                        b.Status != BookingStatus.Provisional &&
+                        b.Status != BookingStatus.CancelledByClient)
+                    .ToListAsync()
+                    .ConfigureAwait(false);
+                return query
+                    .Select(b => new ReadBooking(b));
+            }
         }
 
-        public Task<IEnumerable<IReadBooking>> GetFutureBookingsForClient(Guid clientId, DateTime cutoffDate)
+        public async Task<IEnumerable<IReadBooking>> GetFutureBookingsForClient(Guid clientId, DateTime cutoffDate)
         {
-            throw new NotImplementedException();
+            using (var context = new BookingEntityContext())
+            {
+                var query = await context.Bookings
+                    .Include("TherapistBookings")
+                    .Where(b =>
+                        b.ProposedTime > cutoffDate &&
+                        b.ClientId == clientId &&
+                        b.Status != BookingStatus.Provisional &&
+                        b.Status != BookingStatus.CancelledByClient)
+                    .ToListAsync()
+                    .ConfigureAwait(false);
+                return query
+                    .Select(b => new ReadBooking(b));
+            }
         }
 
-        public Task<IEnumerable<IReadBooking>> GetFutureBookingsForTherapist(Guid therapistId, DateTime cutoffDate)
+        public async Task<IEnumerable<IReadBooking>> GetFutureBookingsForTherapist(Guid therapistId, DateTime cutoffDate)
         {
-            throw new NotImplementedException();
+            using (var context = new BookingEntityContext())
+            {
+                var query = await context.TherapistBookings
+                    .Include("Booking")
+                    .Where(tb =>
+                        tb.Booking.ProposedTime > cutoffDate &&
+                        tb.TherapistId == therapistId &&
+                        tb.Booking.Status != BookingStatus.Provisional &&
+                        tb.Booking.Status != BookingStatus.CancelledByClient)
+                    .Select(tb => tb.Booking)
+                    .ToListAsync()
+                    .ConfigureAwait(false);
+                return query
+                    .Select(b => new ReadBooking(b));
+            }
         }
 
-        public Task AddBooking(Guid bookingId, Guid clientId, DateTime proposedTime, TimeSpan duration)
+        public async Task AddBooking(Guid bookingId, Guid clientId, DateTime proposedTime, TimeSpan duration)
         {
-            throw new NotImplementedException();
+            using (var context = new BookingEntityContext())
+            {
+                var booking =
+                    new DbBooking
+                    {
+                        BookingId = (bookingId != Guid.Empty) ? bookingId : Guid.NewGuid(),
+                        Status = BookingStatus.Provisional,
+                        ClientId = clientId,
+                        ProposedTime = proposedTime,
+                        Duration = duration
+                    };
+                context.Bookings.Add(booking);
+                await context.SaveChangesAsync().ConfigureAwait(false);
+            }
         }
 
-        public Task UpdateBooking(Guid bookingId, BookingStatus status, DateTime proposedTime, TimeSpan duration)
+        public async Task UpdateBooking(Guid bookingId, BookingStatus status, DateTime proposedTime, TimeSpan duration)
         {
-            throw new NotImplementedException();
+            using (var context = new BookingEntityContext())
+            {
+                var booking = await context.Bookings.FindAsync(bookingId).ConfigureAwait(false);
+                booking.Status = status;
+                booking.ProposedTime = proposedTime;
+                booking.Duration = duration;
+            }
         }
     }
 }
--- src/Zen.Massage.Site/SiteIocModule.cs
+++ src/Zen.Massage.Site/SiteIocModule.cs
@@ -3,13 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Autofac;
+using NEventStore;
+using Zen.Infrastructure.ReadRepository;
+using Zen.Massage.Domain.BookingContext;
 
 namespace Zen.Massage.Site
 {
     public class SiteIocModule : Module
     {
+        // TODO: Pass ASP.NET configuration object into module
+        //  so setup parameters can be setup via IoC
+
         protected override void Load(ContainerBuilder builder)
         {
+            // Setup event store
+            var store = Wireup.Init()
+                .UsingInMemoryPersistence() // TODO: Switch to Azure document DB persistence scheme
+                .UsingJsonSerialization()
+                .Compress()
+                //.EncryptWith()                // TODO: Enable encryption (on a per-tenant basis eventually)
+                .Build();
+            builder.RegisterInstance(store);
+
+            // Register core domain types
+            builder.RegisterType<BookingFactory>()
+                .As<IBookingFactory>();
+
+            // Register infrastructure types
+            builder.RegisterType<BookingReadRepository>()
+                .As<IBookingReadRepository>();
+            builder.RegisterType<IBookingWriteRepository>()
+                .As<IBookingWriteRepository>();
+
+            // Register application types
+
+            // Register site types
         }
     }
 }
