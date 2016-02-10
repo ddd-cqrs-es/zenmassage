7ff7061104c95206e41cae05e6207753ebfb5c4e
DementedDevil <zen_global@hotmail.com>
2016-02-10 14:24:03 +0000

    Added placeholder support for nanomessagebus as the communication mechanism between layers.

--- src/Zen.Massage.Site/SiteIocModule.cs
+++ src/Zen.Massage.Site/SiteIocModule.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Autofac;
+using NanoMessageBus;
 using NEventStore;
 using Zen.Infrastructure.ReadRepository;
 using Zen.Massage.Domain.BookingContext;
+using Module = Autofac.Module;
 
 namespace Zen.Massage.Site
 {
@@ -16,12 +19,19 @@ namespace Zen.Massage.Site
 
         protected override void Load(ContainerBuilder builder)
         {
+            // Setup messagebus (we're using lightweight message bus rather than Azure ServiceBus)
+            var messagingHost = new MessagingWireup()
+                .WithAuditing()
+                .StartWithReceive(new AutofacRoutingTable(builder, Assembly.GetExecutingAssembly()));
+
             // Setup event store
             var store = Wireup.Init()
-                .UsingInMemoryPersistence() // TODO: Switch to Azure document DB persistence scheme
+                .UsingInMemoryPersistence()     // TODO: Switch to SQL connection for persistence
                 .UsingJsonSerialization()
                 .Compress()
                 //.EncryptWith()                // TODO: Enable encryption (on a per-tenant basis eventually)
+                .HookIntoPipelineUsing()        // TODO: Hook into processing pipeline so we can route events
+                                                //  through the application message bus queue
                 .Build();
             builder.RegisterInstance(store);
 
