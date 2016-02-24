7f5a89dc4d84f46f4469e4b472e69d1f50e7ca68
Adrian Lewis <zen_global@hotmail.com>
2016-02-24 19:42:58 +0000

    Customer/therapist changes

---
+++ src/Zen.Massage.Domain/UserContext/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zen.Massage.Domain.UserContext
+{
+    public interface ICustomer
+    {
+        
+    }
+
+    public interface ITherapist : ICustomer
+    {
+        
+    }
+
+    public class Customer : ICustomer
+    {
+    }
+
+    public class Therapist : Customer, ITherapist
+    {
+        
+    }
+}
