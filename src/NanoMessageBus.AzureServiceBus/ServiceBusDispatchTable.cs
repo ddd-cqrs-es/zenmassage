c8437bf9842f0584ff10f761594bc96104d54fc9
DementedDevil <zen_global@hotmail.com>
2016-03-01 07:25:42 +0000

    Misc changes to dispatch table Revised message adapter implementation.

--- src/NanoMessageBus.AzureServiceBus/ServiceBusDispatchTable.cs
+++ src/NanoMessageBus.AzureServiceBus/ServiceBusDispatchTable.cs
@@ -7,22 +7,27 @@ namespace NanoMessageBus.Channels
     {
         public ICollection<Uri> this[Type messageType]
         {
-            get { throw new NotImplementedException(); }
+            get
+            {
+                if (messageType == null)
+                {
+                    throw new ArgumentNullException(nameof(messageType));
+                }
+
+                return new[] { new Uri("fanout://" + messageType.FullName, UriKind.Absolute) };
+            }
         }
 
         public void AddSubscriber(Uri subscriber, Type messageType, DateTime expiration)
         {
-            throw new NotImplementedException();
         }
 
         public void AddRecipient(Uri recipient, Type messageType)
         {
-            throw new NotImplementedException();
         }
 
         public void Remove(Uri address, Type messageType)
         {
-            throw new NotImplementedException();
         }
     }
 }
